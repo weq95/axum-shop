1f83b9a1d366c25b867c7e6a88e45590364dcf2e
_dyl <33545159+weq95@users.noreply.github.com>
2024-01-17 15:45:18 +0800

    Update mod.rs

--- admin/src/middleware/mod.rs
+++ admin/src/middleware/mod.rs
@@ -2,7 +2,7 @@ use axum::headers::authorization::Bearer;
 use axum::headers::Authorization;
 use axum::http::{Request, StatusCode};
 use axum::middleware::Next;
-use axum::response::Response;
+use axum::response::{IntoResponse, Response};
 use axum::TypedHeader;
 
 use common::error::{ApiError, ApiResult};
@@ -21,25 +21,21 @@ pub async fn auth_guard<B>(
         Ok(claims) => {
             // 验证redis 用户登录信息
             if claims.token_type() != "access_token" && req.uri().path() != "/refresh_token" {
-                return Err(ApiError::Error(
-                    ApiResponse::<i32>::fail_msg_code(
-                        StatusCode::UNAUTHORIZED.as_u16(),
-                        "非法的token".to_string(),
-                    )
-                    .to_string(),
-                ));
+                return Ok(ApiResponse::<i32>::fail_msg_code(
+                    StatusCode::UNAUTHORIZED.as_u16(),
+                    "非法的token".to_string(),
+                )
+                .into_response());
             }
 
             // 携带用户信息到下游去
             req.extensions_mut().insert(claims);
             Ok(next.run(req).await)
         }
-        Err(_e) => Err(ApiError::Error(
-            ApiResponse::<i32>::fail_msg_code(
-                StatusCode::UNAUTHORIZED.as_u16(),
-                "您还未登录系统".to_string(),
-            )
-            .to_string(),
-        )),
+        Err(_e) => Ok(ApiResponse::<i32>::fail_msg_code(
+            StatusCode::UNAUTHORIZED.as_u16(),
+            "您还未登录系统".to_string(),
+        )
+        .into_response()),
     }
 }
